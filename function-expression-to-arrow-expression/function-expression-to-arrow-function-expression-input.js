552b5c0f3fb0ade6fbc8afa74fff69303765869c
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-04-14 17:03:21 +0100

    function-expression-to-arrow-function-expression-input.js

--- function-expression-to-arrow-expression/function-expression-to-arrow-function-expression-input.js
+++ function-expression-to-arrow-expression/function-expression-to-arrow-function-expression-input.js
@@ -1,42 +1,42 @@
 /*
  * Converts a FunctionExpression to an ArrowFunctionExpression when safe to do so.
-*/
-var a = function(a, b) {
-	return a + b;
-}
-var b = function(a, b) {
- 	var c = 0;
-  	return a + b + c;
-}
-var a = function(a, b) {
- 	return a + b + this.c;
-}
+ */
+var a = function (a, b) {
+  return a + b;
+};
+var b = function (a, b) {
+  var c = 0;
+  return a + b + c;
+};
+var a = function (a, b) {
+  return a + b + this.c;
+};
 
-var a = (a, b) => a + b
+var a = (a, b) => a + b;
 var b = (a, b) => {
- 	var c = 0;
-  	return a + b + c;
-}
-var a = function(a, b) {
- 	return a + b + this.c;
-}
+  var c = 0;
+  return a + b + c;
+};
+var a = function (a, b) {
+  return a + b + this.c;
+};
 
-var a = function(a, b) {
-	return a + b;
-}
-var b = function(a, b) {
- 	var c = 0;
-  	return a + b + c;
-}
-var a = function(a, b) {
- 	return a + b + this.c;
-}
+var a = function (a, b) {
+  return a + b;
+};
+var b = function (a, b) {
+  var c = 0;
+  return a + b + c;
+};
+var a = function (a, b) {
+  return a + b + this.c;
+};
 
-var a = (a, b) => a + b
+var a = (a, b) => a + b;
 var b = (a, b) => {
- 	var c = 0;
-  	return a + b + c;
-}
-var a = function(a, b) {
- 	return a + b + this.c;
-}
+  var c = 0;
+  return a + b + c;
+};
+var a = function (a, b) {
+  return a + b + this.c;
+};
